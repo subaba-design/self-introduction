87da7ae4398b4ab3f57e485819d5b00e199fe385
Masaya Fukai <f.m.11404@gmail.com>
2021-02-21 09:17:05 +0900

    Create : Day2

--- src/components/Strength/Strength.jsx
+++ src/components/Strength/Strength.jsx
@@ -5,6 +5,55 @@ const Strength = () => {
     return (
         <div className="Strength">
             <h1 className="strength-title">Strength</h1>
+            <ul className="strength-sec-list">
+                <li className="strength-list01">
+                    <div className="strength-list-ttl-area">
+                        <p className="strength-list-icon-side">ICON</p>
+                        <p className="strength-list-ttl-eng">自走力</p>
+                        <p className="strength-list-ttl-jpn">Self-management</p>
+                    </div>
+                    <div className="strength-sec-lead-area">
+                        <p>何もない状態から何かを達成するためには、
+                            まず目標の仮説を立て、膨大な情報収集を行います。
+                            何かの問題を改善する時には、どこに問題があるかを突き止めます。
+                            これにより現在地と目的地とのギャップが明確化でき、
+                            ここから達成のための効率のいいやり方を探します。
+                            独学がいいのか、誰かに教わった方がいいのか。
+                            計画が決まれば後は行動あるのみです。やるか、やるか、やるか。
+                        </p>
+                    </div>
+                </li>
+                <li className="strength-list02">
+                    <div className="strength-list-ttl-area">
+                        <p className="strength-list-icon-side">ICON</p>
+                        <p className="strength-list-ttl-eng">デザイン力</p>
+                        <p className="strength-list-ttl-jpn">Design</p>
+                    </div>
+                    <div className="strength-sec-lead-area">
+                        <p>芸大の大学受験のために、1年の浪人を含み2年間画塾に通っていました。
+                            デッサン、色彩構成、立体作品を100点以上作り、本番こそ上手くいきませんでしたが、
+                            50名以上を抱える画塾のパンフレットの表紙を飾っていただいていました。
+                            基本的な補色効果や明度・彩度効果などを理解していますし、
+                            どう表現したら鑑賞側がどう感じるか、という問いを悩み抜き、考え続けた経験があります
+                        </p>
+                    </div>
+                </li>
+                <li className="strength-list03">
+                    <div className="strength-list-ttl-area">
+                        <p className="strength-list-icon-side">ICON</p>
+                        <p className="strength-list-ttl-eng">根気強さ</p>
+                        <p className="strength-list-ttl-jpn">Perseverance</p>
+                    </div>
+                    <div className="strength-sec-lead-area">
+                        <p>高校の３年間は合気道部に所属し、主将兼部長を務めていました。
+                            比較的激しい部活だったため、足の靭帯を損傷し、松葉杖をついていた時期もありましたが、
+                            ３年間やり通して、有段者資格と黒帯をいただきました。
+                            一度やり始めると、最後までやり切らないと納得ができません。
+                            ここで培った忍耐力や根気強さは自信がありますし、プログラミングに限らずともあらゆる面で活きていると思います。
+                        </p>
+                    </div>
+                </li>
+            </ul>
         </div>
     );
 }
