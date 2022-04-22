1ee220a86cc57e310550d06fdc4d98325855e843
subaba <subaba199@gmail.com>
2022-04-22 19:13:19 +0900

    modified HEADER by subaba.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -21,7 +21,6 @@ const Header = (props) => {
         MENU_Contact : 'Contact',
         LINK_Twitter : 'Twitter',
         LINK_Blog    : 'Blog',
-        LINK_Qiita   : 'Qiita',
         LINK_GitHub  : 'GitHub'
     });
 
@@ -80,22 +79,17 @@ const Header = (props) => {
             </List>
             <Divider />
             <List>
-                <a href="https://twitter.com/yopipo415" className="header-list-item">
+                <a href="https://twitter.com/subaba_design" className="header-list-item">
                     <ListItem button key={ state.LINK_Twitter }>
                         <ListItemText primary={ state.LINK_Twitter } />
                     </ListItem>
                 </a>
-                <a href="https://let-there-be-magic.com/" className="header-list-item">
+                <a href="https://subaba-design.com" className="header-list-item">
                     <ListItem button key={ state.LINK_Blog }>
                         <ListItemText primary={ state.LINK_Blog } />
                     </ListItem>
                 </a>
-                <a href="https://qiita.com/Yopipo415" className="header-list-item">
-                    <ListItem button key={ state.LINK_Qiita }>
-                        <ListItemText primary={ state.LINK_Qiita } />
-                    </ListItem>
-                </a>
-                <a href="https://github.com/fslm5415" className="header-list-item">
+                <a href="https://github.com/subaba-design" className="header-list-item">
                     <ListItem button key={ state.LINK_GitHub }>
                         <ListItemText primary={ state.LINK_GitHub } />
                     </ListItem>
